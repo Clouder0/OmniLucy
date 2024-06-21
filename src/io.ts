87e5ed05d5b4978438cd643d4b156ffabec51712
Clouder0 <clouder0@outlook.com>
2024-06-22 01:50:20 +0800

    fix(channel): fix type inference

--- src/io.ts
+++ src/io.ts
@@ -14,21 +14,9 @@ export type HyperBlockCapability = {
 export type HyperTextCapability = {
   text?: boolean;
   image?: boolean;
-  hyperBlock?: boolean;
-  hyperBlockContent?: HyperBlockCapability;
+  hyperBlock?: HyperBlockCapability;
 };
-export type InputCapability = {
-  text?: boolean;
-  image?: boolean;
-  hyperText?: boolean;
-  hyperTextContent?: HyperTextCapability;
-};
-export type OutputCapability = {
-  text?: boolean;
-  image?: boolean;
-  hyperText?: boolean;
-  hyperTextContent?: HyperTextCapability;
-};
+
 export type HyperBlockFromCapability<T extends HyperBlockCapability> =
   | (T extends { text: true } ? Text : never)
   | (T extends { image: true } ? Image : never);
@@ -36,54 +24,62 @@ export type HyperBlockFromCapability<T extends HyperBlockCapability> =
 export type HyperSegmentFromCapability<T extends HyperTextCapability> =
   | (T extends { text: true } ? Text : never)
   | (T extends { image: true } ? Image : never)
-  | (T extends { hyperBlock: true; hyperBlockContent: HyperBlockCapability }
-    ? HyperBlockFromCapability<T["hyperBlockContent"]>
+  | (T["hyperBlock"] extends HyperBlockCapability ? (HyperBlockFromCapability<T["hyperBlock"]>)[]
     : never);
 export type HyperTextFromCapability<T extends HyperTextCapability> = HyperSegmentFromCapability<T>[];
 
+export type InputCapability = {
+  text?: boolean;
+  image?: boolean;
+  hyperText?: HyperTextCapability;
+};
+
 export type InputContentFromCapability<T extends InputCapability> =
-  | (T extends { text: false | undefined } ? never : Text)
-  | (T extends { image: false | undefined } ? never : Image)
-  | (T extends { hyperText: true; hyperTextContent: HyperTextCapability }
-    ? HyperTextFromCapability<T["hyperTextContent"]>
+  | (T["text"] extends true ? Text : never)
+  | (T["image"] extends true ? Image : never)
+  | (T["hyperText"] extends HyperTextCapability ? HyperTextFromCapability<T["hyperText"]>
     : never);
 
 export type InputFromCapability<T extends InputCapability> =
-  | (Text extends InputContentFromCapability<T> ? {
+  | (T["text"] extends true ? {
       content_type: "text";
       content: Text;
     }
     : never)
-  | (Image extends InputContentFromCapability<T> ? {
+  | (T["image"] extends true ? {
       content_type: "image";
       content: Image;
     }
     : never)
-  | (HyperText extends InputContentFromCapability<T> ? {
+  | (T["hyperText"] extends HyperTextCapability ? {
       content_type: "hyperText";
       content: HyperText & InputContentFromCapability<T>;
     }
     : never);
 
+export type OutputCapability = {
+  text?: boolean;
+  image?: boolean;
+  hyperText?: HyperTextCapability;
+};
 export type OutputContentFromCapability<T extends OutputCapability> =
-  | (T extends { text: false | undefined } ? never : Text)
-  | (T extends { image: false | undefined } ? never : Image)
-  | (T extends { hyperText: true; hyperTextContent: HyperTextCapability }
-    ? HyperTextFromCapability<T["hyperTextContent"]>
+  | (T["text"] extends true ? Text : never)
+  | (T["image"] extends true ? Image : never)
+  | (T extends ({ hyperText: HyperTextCapability }) ? HyperTextFromCapability<T["hyperText"]>
     : never);
 
 export type OutputFromCapability<T extends OutputCapability> =
-  | (Text extends OutputContentFromCapability<T> ? {
+  | (T["text"] extends true ? {
       content_type: "text";
       content: Text;
     }
     : never)
-  | (Image extends OutputContentFromCapability<T> ? {
+  | (T["image"] extends true ? {
       content_type: "image";
       content: Image;
     }
     : never)
-  | (HyperText extends OutputContentFromCapability<T> ? {
+  | (T["hyperText"] extends HyperTextCapability ? {
       content_type: "hyperText";
       content: HyperText & OutputContentFromCapability<T>;
     }
